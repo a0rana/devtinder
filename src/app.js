ed43ad3243ab6421fc65362f0f2c910942451b7d
Anurag Rana <anurag.rana@oracle.com>
2024-10-07 20:22:14 +0530

    multiple action handler and dynamic route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.send('test');
+app.get('/test/:id', (req, res) => {
+    console.log(req.params);
+    res.send('get test');
 });
 
-app.post('/hello', (req, res) => {
-    res.send('hello');
+app.post('/test', (req, res) => {
+    res.send('posted');
+});
+
+app.patch('/test', (req, res) => {
+    res.send('patched');
+});
+
+app.put('/test', (req, res) => {
+    res.send('updated');
+});
+
+app.delete('/test', (req, res) => {
+    res.send('deleted');
 });
 
 app.use('/', (req, res) => {
