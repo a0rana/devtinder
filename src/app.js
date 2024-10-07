d81658bbb6e9e1d921e9bb51061c1c2c6e2945cb
Anurag Rana <anurag.rana@oracle.com>
2024-10-07 20:32:16 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.get('/test/:id', (req, res) => {
-    console.log(req.params);
-    res.send('get test');
-});
+app.get('/test', (req, res, next) => {
+        console.log('route handler 1');
+        //res.send('response 1');
+        next();
+    },
+    [(req, res, next) => {
+        console.log('route handler 2');
+        res.send('response 2');
+        //next();
+    },
+    (req, res, next) => {
+        console.log('route handler 3');
+        //res.send('response 3');
+        next();
+    }],
+    (req, res, next) => {
+        console.log('route handler 4');
+        // next();
+        res.send('response 4');
+    });
 
 app.post('/test', (req, res) => {
     res.send('posted');
@@ -23,9 +39,9 @@ app.delete('/test', (req, res) => {
     res.send('deleted');
 });
 
-app.use('/', (req, res) => {
+/*app.use('/', (req, res) => {
     res.send('slash slash slash');
-});
+});*/
 
 app.listen(7777, () => {
     console.log('Started server at port number 7777 .....');
