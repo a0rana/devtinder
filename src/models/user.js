e6486aebeaceab7673664e4b023371940d3e306c
Anurag Rana <anurag.rana@oracle.com>
2024-10-15 19:37:24 +0530

    added schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,54 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    'firstName':{
-        type: String
+    'firstName': {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50
     },
-    'lastName':{
-        type: String
+    'lastName': {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50
     },
-    'email':{
-        type: String
+    'email': {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     'password': {
-        type: String
+        type: String,
+        required: true,
+        validate: function() {
+            if(this.password.length < 5){
+                throw new Error('Password must be at least 6 characters');
+            }
+        }
     },
     'age': {
-        type: Number
+        type: Number,
+        required: true,
+        min: 18,
+        max: 60
     },
-    'gender':{
-        type: Number
+    'gender': {
+        type: String,
+        enum: ['male', 'female', 'other'],
+        required: true
+    },
+    'photoUrl': {
+        type: String,
+        default: 'https://s3.amazonaws.com/photos/',
+    },
+    'skills': {
+        type: [String],
     }
-}, {timestamps: true});
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('User', userSchema);
